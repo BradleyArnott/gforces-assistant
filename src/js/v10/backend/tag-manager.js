46676249d8d2e25b3a11f933e8b5bb7ff98b655a
Beau August <beau.august@gforces.co.uk>
2017-08-04 17:07:25 +0100

    Added more scripts

--- src/js/v10/backend/tag-manager.js
+++ src/js/v10/backend/tag-manager.js
@@ -1,7 +1,5 @@
-$('<a title="Add" id="add-css-tag" href="#" class="btn btn-success css-assistant--button">Use predefined code</a>').insertAfter('#tag-edit');
-var tagManager = {};
-
-const cssTagsJS = [
+var tagManager = {},
+	cssTagsJS = [
 	{
 		id: 0,
 		name: 'JS-Absolute',
@@ -21,16 +19,55 @@ const cssTagsJS = [
 		id: 3,
 		name: 'Miappi',
 		code: ''
+	},
+	{
+		id: 4,
+		name: 'HR Spacing',
+		code: ''
+	},	
+	{
+		id: 5,
+		name: 'New Car Detail (Desktop-first)',
+		code: ''
+	},	
+	{
+		id: 6,
+		name: 'New Car Detail (User-first)',
+		code: ''
+	},	
+	{
+		id: 7,
+		name: 'New Car Dropdown',
+		code: ''
+	},
+	{
+		id: 8,
+		name: 'Add class to body',
+		code: ''
+	},
+	{
+		id: 9,
+		name: 'Current Year',
+	},
+	{
+		id: 10,
+		name: 'Open in new tab',
+		code: ''
 	}
 ];
 
 tagManager.init = function() {
+	$('<a title="Add" id="add-css-tag" href="#" class="btn btn-success css-assistant--button">Use predefined code</a>').appendTo('body');
 	$('#add-css-tag').click(function(e) {
 		e.preventDefault();
 		tagManager.populate().then(function() {
 			tagManager.scriptClick();
 		});
 	})
+}  
+
+function isEmpty( el ){
+	return !$.trim(el.html())
 }
 
 tagManager.populate = function() {
@@ -40,7 +77,6 @@ tagManager.populate = function() {
 		for (let t = 0; t < cssTagsJS.length; t++) {
 			let scriptName = cssTagsJS[t].name,
 				scriptID = cssTagsJS[t].id;
-			console.log(t)
 			$('<a href="#"" class="script" data-id="' + scriptID + '"><div class="inner">' + scriptName + '</div></a>').appendTo(scriptContainer);
 		}
 		$(scriptContainer).appendTo('body');
@@ -51,7 +87,7 @@ tagManager.populate = function() {
 tagManager.scriptClick = function() {
 	$('.custom-css-js--container .script').click(function(e) {
 		e.preventDefault();
-		console.log($(this).attr('data-id'));
+		console.log('CSS - ' + $(this).find('.inner').text())
 	})
 }
 
