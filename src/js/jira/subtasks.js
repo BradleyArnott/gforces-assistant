3fb22588d62a6b8e8bb254c3c7f4b300d7b105cb
Beau August <beau.august@gforces.co.uk>
2017-10-18 13:56:31 +0100

    jQuery dependancy removed from subtasks script

--- src/js/jira/subtasks.js
+++ src/js/jira/subtasks.js
@@ -1,112 +1,129 @@
-var subtasks = {},
-	subtasksToDo = {
-		'Queued': '#C8E1FB',
-		'Not Started': '#C8E1FB',
-		'Quote requested': '#C8E1FB',
-		'Quoted': '#C8E1FB',
-		'In Progress': '#BAF3C3',
-		'More information': '#FFFFD5'
-	},
-	subtasksDone = [
-		'Closed',
-		'Resolved',
-		'Rejected'
-	],
-	CSSLabels = {
-		'repeatissue': '#ffb355',
-		'css-qa-config': '#7e82ff',
-		'css-qa-content': '#7e82ff',
-		'css-qa-setup': '#7e82ff',
-		'css-qa-dev': '#ffa4a4',
-		'css-core': '#d5d832',
-		'css-scope-change': '#ff6c6c'
-	},
-	subtasksDoneColour = '#b2d8b9';
+const subtasks = {
+    table: document.querySelector('#issuetable'),
+    toDo: {
+        Queued: '#C8E1FB',
+        'Not Started': '#C8E1FB',
+        'Quote requested': '#C8E1FB',
+        Quoted: '#C8E1FB',
+        'In Progress': '#BAF3C3',
+        'More information': '#FFFFD5',
+    },
+    done: '#b2d8b9',
+    doneLabels: [
+        'Closed',
+        'Resolved',
+        'Rejected',
+    ],
+    labels: {
+        repeatissue: '#ffb355',
+        'css-qa-config': '#7e82ff',
+        'css-qa-content': '#7e82ff',
+        'css-qa-setup': '#7e82ff',
+        'css-qa-dev': '#ffa4a4',
+        'css-core': '#d5d832',
+        'css-scope-change': '#ff6c6c',
+    },
 
-$.extend($.expr[':'], {
-	'contains': function(elem, i, match, array) {
-		return (elem.textContent || elem.innerText || '').toLowerCase()
-		.indexOf((match[3] || "").toLowerCase()) >= 0;
-	}
-});
+    init() {
+        settings.get('checkSubTasks').then((checkSubTasks) => {
+            if (!checkSubTasks) return;
+            this.loopList();
+            this.addButton();
+            this.createKey();
+        });
+    },
 
-subtasks.init = function() {
-	Settings.get('checkSubTasks').then(function(checkSubTasks) {
-		if (!checkSubTasks) return;
-		subtasks.loopList();
-		subtasks.addButton();
-		subtasks.createKey();
-	});
-}
+    loopList() {
+        const rows = this.table.querySelectorAll('.issuerow');
 
-subtasks.loopList = function() {
-	$('#issuetable .issuerow').each(function() {
-		var $this = $(this),
-			taskStatus = $this.find(".status span").html();
+        rows.forEach((row) => {
+            const status = row.querySelector('.status span').innerHTML;
 
-		subtasks.identifyCSS($this, taskStatus);
-		subtasks.hideTeam($this, 'PM');
-		subtasks.checkComplete($this, taskStatus);
-		subtasks.checkLabels($this);
-	});
-}
+            this.identifyCSS(row, status);
+            this.hideTeam(row, 'PM');
+            this.checkComplete(row, status);
+            this.checkLabels(row);
+        });
+    },
 
-subtasks.checkLabels = function(el) {
-	var labelEl = el.find('.labels-wrap > .labels');
-	if (labelEl.is('span')) return;
-	labelEl.find('a').each(function() {
-		var label = $(this).attr('title');
-		for (lbl in CSSLabels) {
-			if (label.indexOf(lbl) !== -1) el.css('background', CSSLabels[lbl]);
-		}
-	});
-}
+    checkLabels(el) {
+        const labelEl = el.querySelector('.labels-wrap > .labels');
+        if (labelEl.nodeName === 'SPAN') return;
+        const links = labelEl.querySelectorAll('a');
 
-subtasks.checkComplete = function(el, value) {
-	arrayPos = $.inArray(value, subtasksDone);
-	if (arrayPos === -1) return;
-	el.hide().css('background', subtasksDoneColour);
-}
+        links.forEach((link) => {
+            const label = link.getAttribute('title');
+            Object.keys(this.labels).forEach((value) => {
+                if (label.indexOf(value) !== -1) el.style.backgroundColor = this.labels[value];
+            });
+        });
+    },
 
-subtasks.hideTeam = function(el, team) {
-	var shouldHide = el.find(".stsummary a").is(':contains("' + team + ' ")');
-	if (!shouldHide) return;
-	el.hide();	
-}
+    checkComplete(el, value) {
+        const arrayPos = this.doneLabels.indexOf(value);
+        if (arrayPos === -1) return;
+        el.style.display = 'none';
+        el.style.backgroundColor = this.done;
+    },
 
-subtasks.identifyCSS = function(el, value) {
-	var isCSS = el.find(".stsummary a").is(':contains("CSS")');
-	if (!isCSS) return;
-	if (!subtasksToDo.hasOwnProperty(value)) return;
-	if (value == 'More information') el.appendTo('#issuetable');
-	el.css('background', subtasksToDo[value]);
-}
 
-subtasks.addButton = function() {
-	$(".page-type-navigator #opsbar-opsbar-transitions").append("<li class=\"toolbar-item\"><a enabled=\"true\" class=\"toolbar-trigger show-all-tasks\"><span class=\"trigger-label\">Show all Sub-Tasks</span></a></li>");
-	$(".show-all-tasks").click(function() {
-		isEnabled = $(this).attr('enabled')
-		if (isEnabled == 'true') {
-			$(this).attr('enabled', 'false');
-			$(this).find("span").text("Hide irrelevant sub-tasks");
-			$('#issuetable .issuerow').css('display', 'table-row');
-			return;
-		}
-		$('#issuetable .issuerow').removeAttr("style");
-		subtasks.loopList();
-		$(this).attr('enabled', 'true');
-		$(this).find("span").text("Show all sub-tasks");
-	});
-}
+    hideTeam(el, team) {
+        const summary = el.querySelector('.stsummary a');
+        const shouldHide = summary.innerHTML.includes(team);
+        if (!shouldHide) return;
+        el.style.display = 'none';
+    },
 
-subtasks.createKey = function() {
-	let list = '<div class="custom-css-key"><div class="key-title"> Label colour key:</div><ul>';
+    identifyCSS(el, value) {
+        const summary = el.querySelector('.stsummary a');
+        const isCSS = summary.innerHTML.includes('CSS');
+        if (!isCSS) return;
+        if (!this.toDo[value]) return;
+        if (value === 'More information') this.table.appendChild(el);
+        el.style.backgroundColor = this.toDo[value];
+    },
 
-	for (const [key, value] of Object.entries(CSSLabels)) {
-		list += `<li><div style="background:${value};"></div><span>${key}</span></li>`;
-	};
-	list += '</div>'
-	$(list).appendTo('.issue-main-column #details-module_heading');
-}
+    addButton() {
+        const button = document.createElement('li');
+        button.className = 'toolbar-item';
+        button.innerHTML = '<a enabled="true" class="toolbar-trigger show-all-tasks"><span class="trigger-label">Show all Sub-Tasks</span></a>';
+        document.querySelector('.page-type-navigator #opsbar-opsbar-transitions').appendChild(button);
+        const btn = button.querySelector('a');
+
+        btn.addEventListener('click', () => {
+            const isEnabled = btn.getAttribute('enabled');
+            if (isEnabled === 'true') {
+                btn.setAttribute('enabled', 'false');
+                btn.querySelector('span').innerHTML = 'Hide irrelevant sub-tasks';
+                $('#issuetable .issuerow').css('display', 'table-row');
+                return;
+            }
+            const rows = this.table.querySelectorAll('.issuerow');
+
+            rows.forEach((row) => {
+                row.removeAttribute('style');
+            });
+
+            this.loopList();
+
+            btn.setAttribute('enabled', 'true');
+            btn.querySelector('span').innerHTML = 'Show all sub-tasks';
+        });
+    },
+
+    createKey() {
+        let list = '<div class="key-title"> Label colour key:</div><ul>';
+
+        Object.entries(this.labels).forEach((label) => {
+            list += `<li><div style="background:${label[1]};"></div><span>${label[0]}</span></li>`;
+        });
+        list += '</ul>';
+
+        const el = document.createElement('div');
+        el.className = 'custom-css-key';
+        el.innerHTML = list;
+        document.querySelector('.issue-main-column #details-module_heading').appendChild(el);
+    },
+};
 
 subtasks.init();
