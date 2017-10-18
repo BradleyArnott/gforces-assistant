40ed5f844e646afde23e5eae77b23c4c38f477c9
Beau August <beau.august@gforces.co.uk>
2017-10-18 12:40:53 +0100

    jQuery removed from projects board scripts

--- src/js/jira/projects.js
+++ src/js/jira/projects.js
@@ -1,124 +1,127 @@
-projects = {
+const projects = {
+    data: [],
 
-	data: [],
+    init() {
+        setTimeout(() => {
+            this.loopIssues()
+                .then(() => {
+                    this.setCookie(this.data, 1);
+                });
+        }, 2000);
+    },
 
-	init: function() {
+    loopIssues() {
+        return new Promise(((resolve) => {
+            const promises = [];
+            const issues = document.querySelectorAll('.ghx-first .ghx-column[data-column-id="1298"] .ghx-issue');
+            const cached = JSON.parse(this.getCookie());
+            if (cached) this.data = JSON.parse(this.getCookie());
+            issues.forEach((issue) => {
+                promises.push(new Promise(((res) => {
+                    if (!cached) {
+                        this.getIssue(issue)
+                            .then(this.applyDays)
+                            .then(() => {
+                                res();
+                            });
+                    } else {
+                        cached.forEach((entry, index) => {
+                            const cachedKey = entry.issue;
+                            const issueKey = issue.getAttribute('data-issue-key');
 
-		setTimeout(function() {
+                            if (cachedKey === issueKey) {
+                                if (!entry.phaseOne) return;
+                                entry.issue = issue;
+                                this.applyDays(entry);
+                                resolve();
+                                return;
+                            }
 
-			projects.loopIssues()
-				.then(function() {
-					console.log(projects.data)
-					projects.setCookie(projects.data, 1);
-				})
+                            if (index === cached.length - 1 && cachedKey !== issueKey) {
+                                this.getIssue(issue)
+                                    .then(this.applyDays)
+                                    .then(() => {
+                                        resolve();
+                                    });
+                            }
+                        });
+                    }
+                })));
+            });
 
-		}, 2000);
-	},
+            Promise.all(promises)
+                .then(() => {
+                    resolve();
+                });
+        }));
+    },
 
-	loopIssues: function() {
+    getCookie() {
+        const name = 'CSS-Days-Phase-1=';
+        const decodedCookie = decodeURIComponent(document.cookie);
+        const ca = decodedCookie.split(';');
 
-		return new Promise(function(resolve, reject) {
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) === ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) === 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return false;
+    },
 
-			let promises = [],
-				issues = $('.ghx-first .ghx-column[data-column-id="1298"] .ghx-issue'),
-				cached = JSON.parse(projects.getCookie());
-			if (cached) projects.data = JSON.parse(projects.getCookie());
+    setCookie(data) {
+        const tomorrow = moment().add(1, 'day').hours(8).minutes(0)
+            .seconds(0)
+            .toDate();
+        const expires = `expires=${tomorrow}`;
+        document.cookie = `CSS-Days-Phase-1=${JSON.stringify(data)};${expires};path=/`;
+    },
 
-			for (let i = 0; i < issues.length; i++) {
-				let issue = issues[i];
+    getDays(issue) {
+        const daysClass = issue.classList[5];
+        const numDays = daysClass.slice(9);
+        return numDays;
+    },
 
-				promises.push(new Promise(function(resolve, reject) {
+    applyDays(data) {
+        if (!data.phaseOne) return;
+        const days = projects.getDays(data.issue);
+        const warn = days === 3 ? ' warn' : '';
+        const over = days > 3 ? ' over' : '';
+        const counter = document.createElement('div');
+        const content = `<div class=""><span>${days}</span></div>`;
+        const placement = data.issue.querySelector('.ghx-issue-content');
 
-					if (!cached) {
-						projects.getIssue(issue)
-							.then(projects.applyDays)
-							.then(function() {
-								resolve();
-							});
+        counter.className = `custom-days-count${warn}${over}`;
+        counter.innerHTML = content;
 
-					} else {
-						for (let p = 0; p < cached.length; p++) {
-							let cachedKey = cached[p].issue,
-								issueKey = $(issue).attr('data-issue-key');
+        placement.parentNode.insertBefore(counter, placement.nextSibling);
+    },
 
-							if (cachedKey === issueKey) {
-								if (!cached[p].phaseOne) continue;
-								cached[p].issue = issue;
-								projects.applyDays(cached[p]);
-								resolve();
-								break;
-							}
+    getIssue(issue) {
+        const key = issue.getAttribute('data-issue-key');
+        const request = new XMLHttpRequest();
 
-							if (p == cached.length - 1 && cachedKey !== issueKey) {
-								projects.getIssue(issue)
-									.then(projects.applyDays)
-									.then(function() {
-										resolve();
-									});
-							}
-						}
-					}
-				}));
-			};
+        return new Promise((resolve) => {
+            request.open('GET', `https://jira.netdirector.co.uk/rest/api/2/issue/${key}`, true);
 
-			Promise.all(promises)
-				.then(function() {
-					resolve();
-				})
-		});
-	},
+            request.onload = () => {
+                if (request.status >= 200 && request.status < 400) {
+                    let resp = JSON.parse(request.response);
+                    const { labels } = resp.fields;
+                    const phaseOne = labels.includes('CSS-QA-Phase1');
+                    this.data.push({ issue: key, phaseOne });
+                    resolve({ issue, phaseOne });
+                }
+            };
 
-	getCookie: function() {
-		let name = "CSS-Days-Phase-1=",
-			decodedCookie = decodeURIComponent(document.cookie),
-			ca = decodedCookie.split(';');
-
-		for(let i = 0; i < ca.length; i++) {
-			let c = ca[i];
-			while (c.charAt(0) == ' ') {
-				c = c.substring(1);
-			}
-			if (c.indexOf(name) == 0) {
-				return c.substring(name.length, c.length);
-			}
-		}
-		return false
-	},
-
-	setCookie: function(data, days) {
-		const tomorrow = moment().add(1, 'day').hours(8).minutes(0).seconds(0).toDate(),
-			expires = "expires="+ tomorrow;
-		document.cookie = 'CSS-Days-Phase-1=' + JSON.stringify(data) + ';' + expires + ';path=/';
-	},
-
-	getDays: function(issue) {
-		const daysClass = issue.classList[5],
-			numDays = daysClass.slice(9);
-
-		return numDays;
-	},
-
-	applyDays: function(data) {
-		if (!data.phaseOne) return;
-		const days = projects.getDays(data.issue),
-			warn = days == 3 ? ' warn' : '',
-			over = days > 3 ? ' over' : '',
-			content = '<div class="custom-days-count' + warn + over + '"><span>' + days + '</span></div>',
-			placement = $(data.issue).find('.ghx-issue-content');
-		$(content).insertAfter(placement);
-	},
-
-	getIssue: function(issue) {
-		const key = $(issue).attr('data-issue-key');
-		return new Promise(function(resolve, reject) {
-			$.get('https://jira.netdirector.co.uk/rest/api/2/issue/' + key, function( data ) {
-				let labels = data.fields.labels,
-					phaseOne = $.inArray('CSS-QA-Phase1', labels) !== -1;
-				projects.data.push({issue: key, phaseOne: phaseOne});
-				resolve({issue: issue, phaseOne: phaseOne});
-			});
-		});
-	}
+            request.send();
+        });
+    },
 };
 
 projects.init();
