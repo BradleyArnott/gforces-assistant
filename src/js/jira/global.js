94f61bcfdab0f10698f183cc389880a637d486ed
Beau August <beau.august@gforces.co.uk>
2017-10-18 11:35:36 +0100

    updated JIRA header button to no longer rely on jQuery

--- src/js/jira/global.js
+++ src/js/jira/global.js
@@ -1,22 +1,23 @@
-var globalJIRA = {};
+const globalJIRA = {
 
-globalJIRA.LabelsButton = function() {
-	let newItem = `	<li>
-						<a class="aui-nav-link aui-dropdown2-trigger " id="ts-custom-labels-link" href="" aria-haspopup="true" aria-owns="ts-custom-labels-link-content" aria-controls="ts-custom-labels-link-content">Automated Labels</a>
-						<div id="ts-custom-labels-link-content" class="aui-dropdown2 aui-style-default aui-dropdown2-in-header" data-dropdown2-alignment="left" aria-hidden="true">
-							<div class="aui-dropdown2-section">
-								<ul id="report" class="aui-list-truncate">
-									<li><a href="/issues/?jql=labels%20%3D%20css-automated-nodue">No Due Date</a></li>
-									<li><a href="/issues/?jql=labels%20%3D%20css-automated-noquote">No Quote</a></li>
-								</ul>
-							</div>
-						</div>
-					</li>`;
-	$(newItem).insertBefore('.aui-header-primary .aui-nav > li:last-child');
-}
+    init() {
+        let newItem = '<a class="aui-nav-link aui-dropdown2-trigger " id="ts-custom-labels-link" href="" aria-haspopup="true" aria-owns="ts-custom-labels-link-content" aria-controls="ts-custom-labels-link-content">Automated Labels</a>';
+        newItem += '<div id="ts-custom-labels-link-content" class="aui-dropdown2 aui-style-default aui-dropdown2-in-header" data-dropdown2-alignment="left" aria-hidden="true">';
+        newItem += '<div class="aui-dropdown2-section">';
+        newItem += '<ul id="report" class="aui-list-truncate">';
+        newItem += '<li><a href="/issues/?jql=labels%20%3D%20css-automated-nodue">No Due Date</a></li>';
+        newItem += '<li><a href="/issues/?jql=labels%20%3D%20css-automated-noquote">No Quote</a></li>';
+        newItem += '</ul>';
+        newItem += '</div>';
+        newItem += '</div>';
 
-globalJIRA.init = function() {
-	globalJIRA.LabelsButton();
-}
+        const position = document.querySelector('.aui-header-primary .aui-nav > li:last-child');
+        const parent = position.parentNode;
+        const menu = document.createElement('li');
+
+        menu.innerHTML = newItem;
+        parent.insertBefore(menu, position);
+    },
+};
 
 globalJIRA.init();
