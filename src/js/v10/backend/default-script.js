66e26ceb6d2fc414aded9277c63426e1be494505
GFORCES\alan.parsons <alan.parsons@gforces.co.uk>
2018-06-15 09:35:31 +0100

    Updated default script and editor CSS

--- src/js/v10/backend/default-script.js
+++ src/js/v10/backend/default-script.js
@@ -4,7 +4,8 @@ nd.auto.require(['jquery', 'externalScriptsLoader'], function($, externalScripts
     // externalScriptsLoader.loadScriptAfterAllRequireScripts('SCRIPT_SRC');
     // check we are not in the editor before running anything
     if ($('[href*="/templateEditor.css"]').length === 0) {
-        console.log('CSS - SCRIPT_NAME');
+        var scriptName = 'SCRIPT_NAME'
+        console.log('CSS - ' . scriptName);
         // START YOUR CODE HERE
     }
 });
